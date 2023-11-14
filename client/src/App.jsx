f58d95f5d8e4e0c59ea36738a71b83a6bbaa3957
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-14 22:47:10 +0200

    folders/files have been moved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import Blog from "./components/Blog/Blog.jsx"
 import Catalog from "./components/Catalog/Catalog.jsx"
 import Footer from "./components/Header/Footer/Footer.jsx"
 import Header from "./components/Header/Header.jsx"
-
+import { Routes, Route } from "react-router-dom"
 
 
 function App() {
   return (
     <>
       <Header />
+
+      {/* <Routes>
+        <Route path='/blog' element={} ></Route>
+      </Routes> */}
 
       <div id="main-wrapper">
         <div className="5grid-layout">
