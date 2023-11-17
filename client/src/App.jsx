3f74753ce1348a82290d71925e986efefb78e883
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-17 12:24:55 +0200

    rename folders and files/components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import Main from "./components/main/Main.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/header/Header.jsx";
 import { Routes, Route } from "react-router-dom";
-import Catalog from "./components/Catalog/Catalog.jsx";
+import Catalog from "./components/catalog/Catalog.jsx";
 import Blog from "./components/Blog/Blog.jsx";
 import About from "./components/about/About.jsx";
 import Login from "./components/login/Login.jsx";
 import Signin from "./components/register/Signin.jsx";
 import { AuthProvider } from "./context/auth-context.jsx";
+import MainPage from "./components/main-page/MainPage.jsx";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <div id="main-wrapper">
         <div className="5grid-layout">
           <Routes>
-            <Route path="/" element={<Main />}></Route>
+            <Route path="/" element={<MainPage />}></Route>
             <Route path="/catalog" element={<Catalog />}></Route>
             <Route path="/blog" element={<Blog />}></Route>
             <Route path="/about" element={<About />}></Route>
