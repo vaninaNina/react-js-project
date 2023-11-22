71ecf103ab40be65ed28db01d71087e2279193d1
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-22 23:16:27 +0200

    Patch added; corrections made

--- client/src/lib/requests.js
+++ client/src/lib/requests.js
@@ -41,5 +41,6 @@ const request = async (method, url, data) => {
 
 export const get = request.bind(null, "GET");
 export const post = request.bind(null, "POST");
-export const put = request.bind(null, "PuT");
+export const put = request.bind(null, "PUT");
 export const del = request.bind(null, "DELETE");
+export const patch = request.bind(null, "PATCH");
