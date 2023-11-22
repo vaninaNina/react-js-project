e9da451d069057663adb1545193996029cae5dae
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-22 23:17:30 +0200

    http requests updated

--- client/src/services/nftService.js
+++ client/src/services/nftService.js
@@ -1,15 +1,21 @@
-const baseUrl = "http://localhost:3030/jsonstore";
+import * as request from "../lib/requests.js";
+
+const baseUrl = "http://localhost:3030/data";
+
+export const getAll = async () => {
+  const result = await request.get(baseUrl);
+
+  return result;
+};
+
+export const getOne = async (nftId) => {
+  const result = await request.get(`${baseUrl}/${nftId}`);
+
+  return result;
+};
 
 export const create = async (nftData) => {
-  const response = await fetch(`${baseUrl}/nfts`, {
-    methos: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(nftData),
-  });
-
-  const result = await response.json();
+  const result = await request.post(baseUrl, nftData);
 
   return result;
 };
