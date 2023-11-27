3a08d48c73cf44e627f2b78bc3f068886c7815dc
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-28 00:50:04 +0200

    blog updated

--- client/src/components/blog/Blog.jsx
+++ client/src/components/blog/Blog.jsx
@@ -1,3 +1,5 @@
+import "../../components/blog/blogstyle.css";
+
 const Blog = () => {
   return (
     <div className="row">
@@ -5,56 +7,10 @@ const Blog = () => {
         <section>
           <div className="5grid">
             <div className="row">
-              <div className="4u">
-                <section className="box">
-                  <a href="#" className="image image-full">
-                    <img src="images/pic09.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Review the catalog</h3>
-                  </header>
-                  <p>
-                    Lorem ipsum dolor sit amet sit veroeros sed amet blandit
-                    consequat veroeros lorem blandit adipiscing et feugiat
-                    phasellus tempus dolore ipsum lorem dolore.
-                  </p>
-                  <footer>
-                    <a href="#" className="button button-alt">
-                      Go to Catalog
-                    </a>
-                  </footer>
-                </section>
-                <section className="box">
-                  <header>
-                    <h3>Content</h3>
-                  </header>
-                  <p>Review the current NTF colletion</p>
-                  <ul className="divided">
-                    <li>
-                      <a href="#">Sed et blandit consequat sed</a>
-                    </li>
-                    <li>
-                      <a href="#">Hendrerit tortor vitae sapien dolore</a>
-                    </li>
-                    <li>
-                      <a href="#">Sapien id suscipit magna sed felis</a>
-                    </li>
-                    <li>
-                      <a href="#">Aptent taciti sociosqu ad litora</a>
-                    </li>
-                    <li>
-                      <a href="#">Aptent taciti sociosqu ad litora</a>
-                    </li>
-                    <li>
-                      <a href="#">Aptent taciti sociosqu ad litora</a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
               <div className="8u mobileUI-main-content">
                 <article className="box is-post">
                   <a href="#" className="image image-full">
-                    <img src="images/pic01.jpg" alt="" />
+                    <img src="images/pic02.jpg" alt="" />
                   </a>
                   <header>
                     <h2>Article 1</h2>
@@ -71,29 +27,16 @@ const Blog = () => {
                     id egestas diam justo adipiscing ante. Pellentesque tempus
                     nulla non urna eleifend ut ultrices nisi faucibus.
                   </p>
-                  <p>
-                    Praesent a dolor leo. Duis in felis in tortor lobortis
-                    volutpat et pretium tellus. Vestibulum ac ante nisl, a
-                    elementum odio. Duis semper risus et lectus commodo
-                    fringilla. Maecenas sagittis convallis justo vel mattis.
-                    placerat, nunc diam iaculis massa, et aliquet nibh leo non
-                    nisl vitae porta lobortis, enim neque fringilla nunc, eget
-                    faucibus lacus sem quis nunc suspendisse nec lectus sit amet
-                    augue rutrum vulputate ut ut mi. Aenean elementum, mi sit
-                    amet porttitor lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Lorem ipsum dolor sit amet nullam consequat
-                    feugiat dolore tempus.
-                  </p>
                   <span>
                     <a href="#" className="button button-alt">
                       Comment
                     </a>
                   </span>
-                  <span>
+                  {/* <span>
                     <a href="#" className="button button-like">
                       Like
                     </a>
-                  </span>
+                  </span> */}
                 </article>
               </div>
               <div className="8u mobileUI-main-content">
@@ -116,29 +59,17 @@ const Blog = () => {
                     id egestas diam justo adipiscing ante. Pellentesque tempus
                     nulla non urna eleifend ut ultrices nisi faucibus.
                   </p>
-                  <p>
-                    Praesent a dolor leo. Duis in felis in tortor lobortis
-                    volutpat et pretium tellus. Vestibulum ac ante nisl, a
-                    elementum odio. Duis semper risus et lectus commodo
-                    fringilla. Maecenas sagittis convallis justo vel mattis.
-                    placerat, nunc diam iaculis massa, et aliquet nibh leo non
-                    nisl vitae porta lobortis, enim neque fringilla nunc, eget
-                    faucibus lacus sem quis nunc suspendisse nec lectus sit amet
-                    augue rutrum vulputate ut ut mi. Aenean elementum, mi sit
-                    amet porttitor lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Lorem ipsum dolor sit amet nullam consequat
-                    feugiat dolore tempus.
-                  </p>
+
                   <span>
                     <a href="#" className="button button-alt">
                       Comment
                     </a>
                   </span>
-                  <span>
+                  {/* <span>
                     <a href="#" className="button button-like">
                       Like
                     </a>
-                  </span>
+                  </span> */}
                 </article>
               </div>
             </div>
