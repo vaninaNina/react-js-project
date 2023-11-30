c75f8964ac3c507aae76e628adfb79dd5455b040
vaninaNina <kalapsazova.vanina@gmail.com>
2023-11-30 22:38:28 +0200

    data service f corrected

--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -2,14 +2,22 @@ import * as request from "../lib/requests.js";
 
 const baseUrl = "http://localhost:3030/data";
 
-export const getBlogPost = async () => {
-  const result = await request.get(`${baseUrl}/blogPost`);
-
-  return result;
-};
-
 export const getFrontText = async () => {
   const result = await request.get(`${baseUrl}/frontText`);
 
   return result;
 };
+
+export const getAllBlogPosts = async () => {
+  const result = await request.get(`${baseUrl}/blogPost`);
+
+  return result;
+};
+
+export const getBlogPost = async (blogPostId) => {
+  const result = await request.get(`${baseUrl}/blogPost/${blogPostId}`);
+  return result;
+};
+
+export const del = async (blogPostId) =>
+  request.del(`${baseUrl}/blogPost/${blogPostId}`);
