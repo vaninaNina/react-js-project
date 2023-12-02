9b0842177671bd001d35ffc337f503458d114c0a
vaninaNina <kalapsazova.vanina@gmail.com>
2023-12-02 12:04:09 +0200

    my profile component changed - to be created

--- client/src/components/myProfile/MyProfile.jsx
+++ client/src/components/myProfile/MyProfile.jsx
@@ -1,5 +1,17 @@
+// import { useContext, useEffect, useState } from "react";
+// import AuthContext from "../../context/authContext.jsx";
+// import * as dataService from "../../services/dataService.js";
+
 const MyProfile = () => {
-  return <h1>My Profile</h1>;
+  // const { userId } = useContext(AuthContext);
+  // const [myPosts, setMyPosts] = useState([]);
+
+  return (
+    <div>
+      <h1>My profile</h1>
+      <section>Empty record</section>
+    </div>
+  );
 };
 
 export default MyProfile;
