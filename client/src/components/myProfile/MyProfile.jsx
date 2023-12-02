30b3885fd9f49668277573b0ac6fba2bc8b63d47
vaninaNina <kalapsazova.vanina@gmail.com>
2023-12-03 01:58:41 +0200

    my blog posts created in my profile component

--- client/src/components/myProfile/MyProfile.jsx
+++ client/src/components/myProfile/MyProfile.jsx
@@ -1,15 +1,42 @@
-// import { useContext, useEffect, useState } from "react";
-// import AuthContext from "../../context/authContext.jsx";
-// import * as dataService from "../../services/dataService.js";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/authContext.jsx";
+import * as dataService from "../../services/dataService.js";
 
 const MyProfile = () => {
-  // const { userId } = useContext(AuthContext);
-  // const [myPosts, setMyPosts] = useState([]);
+  const { userId } = useContext(AuthContext);
+  const [myBlogPosts, setMyBlogPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogPosts = async () => {
+      try {
+        const response = await dataService.getAllBlogPosts();
+
+        const userPosts = response.filter(
+          (blogPost) => blogPost._ownerId === userId,
+        );
+
+        setMyBlogPosts(userPosts);
+      } catch (error) {
+        console.error("Error fetching blog posts:", error);
+      }
+    };
+
+    fetchBlogPosts();
+  }, [userId]);
 
   return (
     <div>
-      <h1>My profile</h1>
-      <section>Empty record</section>
+      <h1>My Blog Posts</h1>
+
+      {myBlogPosts.length <= 0 ? (
+        <p>Empty record</p>
+      ) : (
+        <ul>
+          {myBlogPosts.map((blogPost) => (
+            <li key={blogPost._id}>{blogPost.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
