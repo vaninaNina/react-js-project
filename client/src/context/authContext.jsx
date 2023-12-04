0de963842722c98b5467e66aea7fe9e78b7ddfde
vaninaNina <kalapsazova.vanina@gmail.com>
2023-12-04 19:00:18 +0200

    validations for login added

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -9,10 +9,27 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [auth, setAuth] = usePersistedState("auth", {});
-
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
   const loginSubmitHandler = async (values) => {
     try {
+      if (
+        !values.email ||
+        !values.password ||
+        (!values.email && !values.password)
+      ) {
+        console.error("Both email and password are required.");
+        return;
+      }
+      if (!regex.test(values.email)) {
+        console.error("This is not a valid email format!");
+        return;
+      }
+      if (values.password.length < 3) {
+        console.error("Password must be more than 3 characters");
+        return;
+      }
       const result = await authService.login(values.email, values.password);
+
       setAuth(result);
 
       localStorage.setItem("accessToken", result.accessToken);
